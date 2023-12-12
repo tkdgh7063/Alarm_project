54d61e50cfbef2f4b480f52c57c1b83e1dedec6a
tkdgh7063 <hoo412298@gmail.com>
2023-12-12 23:15:32 +0900

    Update time.c
    added main function

--- RTC/time.c
+++ RTC/time.c
@@ -1,2 +1,25 @@
-import "RTC.h"
+#include "RTC.h"
+#include "../FND/fnd_reg.h"
 
+// RTC main
+void main() {
+    // 시간 출력
+    // fork()
+
+    // RTC_init();
+    // FND_init();
+    
+    int hour;
+    int min;
+    
+    while (1) {
+        hour = getHour();
+        min = getMin();
+
+        display(hour, min);
+        
+        delay(100); // 0.1초 대기
+    }
+
+    return 0;
+}
